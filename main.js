45e66053b5a6754c9b335e34db95cb97fabce47f
080812 <74765678+080812@users.noreply.github.com>
2021-10-07 14:16:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,41 @@
 noseX=0
 noseY=0
 img=""
+
+
 function preload(){
-img=loadImage('ClownWig.png')
+img=loadImage('https://i.postimg.cc/JntqBdt7/Mustache.png');
 }
 
 function setup(){
-canvas=createCanvas(300,300)
-canvas.center()
-video=createCapture(VIDEO)
-video.hide()
-posenet=ml5.poseNet(video,modelloaded)
-posenet.on('pose',gotposes)
+canvas=createCanvas(300,300);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+posenet=ml5.poseNet(video,modelLoaded);
+posenet.on('pose',gotposes);
+
 }
 
 function draw(){
-image(video,0,0,300,300)
-fill("aqua")
-stroke("pink")
-circle(noseX,noseY,30)
-//image(img,noseX,noseY,50,50)
+    image(video,0,0,300,300);
+
+    image(img,noseX,noseY,80,80);
 }
 
-function modelloaded(){
-    console.log("model is loaded")
+function modelLoaded(){
+    console.log("model is loaded");
 }
 
-function gotposes(results){
-    if(results.length>0){
-        console.log(results)
+function gotposes(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
 
-        noseX=results[0].pose.nose.x
-        noseY=results[0].pose.nose.y
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log(noseX,noseY);
     }
 }
